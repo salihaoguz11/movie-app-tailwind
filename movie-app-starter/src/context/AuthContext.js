7b53c08f4ae7eab44f2137b575b5680c181aa770
Saliha <salihaoguz2011@gmail.com>
2023-03-14 11:45:35 +0000

    navigate

--- movie-app-starter/src/context/AuthContext.js
+++ movie-app-starter/src/context/AuthContext.js
@@ -1,9 +1,36 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React, { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../auth/firebase";
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-  const values = { currentUser: { displayName: "Saliha" } };
+  const navigate = useNavigate();
+  const createUser = async (email, password) => {
+    try {
+      let userCredantial = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredantial);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const signIn = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const values = { createUser, signIn, currentUser: { displayName: "Saliha" } };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
