f90bdbd251d4d0988278c0629601e1891a513517
Saliha <salihaoguz2011@gmail.com>
2023-03-14 08:47:48 +0000

    dark mode

--- movie-app-starter/src/pages/Register.jsx
+++ movie-app-starter/src/pages/Register.jsx
@@ -4,8 +4,8 @@ import GoogleIcon from "../assets/icons/GoogleIcon";
 const Register = () => {
   return (
     <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
-      <div className={` form-container mt-[5vh] w-[380px] h-[580px]`}>
-        <form className="absolute inset-[2px] rounded-[8px] bg-gray-100 dark:bg-[#28292d] z-[10] flex flex-col py-[50px] px-[40px]">
+      <div className={`form-container mt-[5vh] w-[380px] h-[580px]`}>
+        <form>
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
