c9bb72fe11142ee182f0f45ff9ca0ae56d26ec7f
Saliha <salihaoguz2011@gmail.com>
2023-03-14 10:18:48 +0000

    authContext

---
+++ movie-app-starter/src/context/AuthContext.js
@@ -0,0 +1,10 @@
+import React, { createContext } from "react";
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+  const values = {};
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContextProvider;
