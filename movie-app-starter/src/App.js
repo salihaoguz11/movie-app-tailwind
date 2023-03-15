888884cf9bd20d587604f95be596a86a2351b0fb
Saliha <salihaoguz2011@gmail.com>
2023-03-15 14:00:07 +0000

    movieContext

--- movie-app-starter/src/App.js
+++ movie-app-starter/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import AuthContextProvider from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
 import { ToastContainer } from "react-toastify";
+import MovieContextProvider from "./context/MovieContext";
 
 const App = () => {
   return (
     <AuthContextProvider>
-      <AppRouter />
-      <ToastContainer />
+      <MovieContextProvider>
+        <AppRouter />
+        <ToastContainer />
+      </MovieContextProvider>
     </AuthContextProvider>
   );
 };
