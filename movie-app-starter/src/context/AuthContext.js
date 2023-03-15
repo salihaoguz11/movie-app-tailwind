9c0a8d2fb07a66007066cfb531cedd72df317db6
Saliha <salihaoguz2011@gmail.com>
2023-03-15 12:25:21 +0000

    userobserver

--- movie-app-starter/src/context/AuthContext.js
+++ movie-app-starter/src/context/AuthContext.js
@@ -1,16 +1,28 @@
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../auth/firebase";
+import {
+  toastErrorNotify,
+  toastSuccessNotify,
+  toastWarnNotify,
+} from "../helpers/ToastNotify";
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState("");
   const navigate = useNavigate();
+
+  useEffect(() => {
+    userObserver();
+  }, []);
+
   const createUser = async (email, password) => {
     try {
       let userCredantial = await createUserWithEmailAndPassword(
@@ -19,28 +31,41 @@ const AuthContextProvider = ({ children }) => {
         password
       );
       navigate("/");
-      console.log(userCredantial);
+      toastSuccessNotify("Registered successfully!");
     } catch (error) {
-      console.log(error);
+      toastErrorNotify(error.message);
     }
   };
   const signIn = async (email, password) => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       navigate("/");
+      toastSuccessNotify("Logged in successfully!");
     } catch (error) {
-      console.log(error);
+      toastErrorNotify(error.message);
     }
   };
+
   const logOut = () => {
     signOut(auth);
   };
 
+  const userObserver = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { email, displayName, photoURL } = user;
+        setCurrentUser({ email, displayName, photoURL });
+      } else {
+        setCurrentUser(false);
+        // console.log("logged out");
+      }
+    });
+  };
   const values = {
     createUser,
     logOut,
     signIn,
-    currentUser: { displayName: "Saliha" },
+    currentUser,
   };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
