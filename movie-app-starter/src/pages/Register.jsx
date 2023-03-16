89dbf1ce65952cf48e0414784290f4b937e82ed1
Saliha <salihaoguz2011@gmail.com>
2023-03-16 13:38:11 +0000

    done

--- movie-app-starter/src/pages/Register.jsx
+++ movie-app-starter/src/pages/Register.jsx
@@ -8,11 +8,25 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { createUser, signUpProvider } = useContext(AuthContext);
+
+  //* birleştirilmiş state
+  // const [info, setInfo] = useState({
+  //   firstName: "",
+  //   lastName: "",
+  //   email: "",
+  //   password: "",
+  // });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const displayName = `${firstName} ${lastName}`;
     createUser(email, password, displayName);
   };
+
+  // const { email, password, firstName, lastName } = info;
+  // const hadleChange = (e) =>
+  //   setInfo({ ...info, [e.target.id]: e.target.value });
+
   return (
     <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
       <div className={`form-container mt-[5vh] w-[380px] h-[580px]`}>
@@ -20,36 +34,46 @@ const Register = () => {
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
-          <div>
+          <div className="relative z-0 w-full mb-6 group">
             <input
-              name="floating_text"
               type="text"
+              name="floating_text"
+              className="peer"
+              placeholder=" "
               required
               onChange={(e) => setFirstName(e.target.value)}
             />
-            <label htmlFor="floating_text">First Name</label>
+            <label htmlFor="floating_email">First Name</label>
           </div>
-          <div>
+          <div className="relative z-0 w-full mb-6 group">
             <input
               name="floating_text"
               type="text"
               required
+              className="peer"
+              placeholder=" "
               onChange={(e) => setLastName(e.target.value)}
             />
             <label htmlFor="floating_text">Last Name</label>
           </div>
-          <div>
+          <div className="relative z-0 w-full mb-6 group">
             <input
               name="floating_email"
               type="email"
+              className="peer"
+              placeholder=" "
+              required
               onChange={(e) => setEmail(e.target.value)}
             />
             <label htmlFor="floating_email">Email</label>
           </div>
-          <div>
+          <div className="relative z-0 w-full mb-6 group">
             <input
               name="floating_password"
               type="password"
+              className="peer"
+              placeholder=" "
+              required
               onChange={(e) => setPassword(e.target.value)}
             />
             <label htmlFor="floating_password">Password</label>
@@ -58,9 +82,9 @@ const Register = () => {
             Register
           </button>
           <button
-            onClick={() => signUpProvider()}
             className="flex justify-between text-center btn-danger"
             type="button"
+            onClick={() => signUpProvider()}
           >
             Continue with Google
             <GoogleIcon color="currentColor" />
